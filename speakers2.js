060b8f80b76fb0477c6f9d5dd78e8ce3ec36ea99
Masahiro Yoshimoto <yoshimoto@onsanai.com>
2025-02-28 19:54:27 +0900

    コピペしやすいようにタイトル一覧の時間と年月日の前後にスペースを追加

--- speakers2.js
+++ speakers2.js
@@ -193,7 +193,7 @@ function displayTitlesImpl(element, titleDatas) {
         const dateSpan = document.createElement("span");
         dateSpan.className = "article-date";
         const date = new Date(titleData.unixtime);
-        dateSpan.textContent = `${formatDuration(titleData.duration)}  ${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
+        dateSpan.textContent = ` ${formatDuration(titleData.duration)}  ${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')} `;
 
         const speakersSpan = document.createElement("span");
         speakersSpan.className = "article-speakers";
