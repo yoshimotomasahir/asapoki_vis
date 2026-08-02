4e0aa72224482239b80cef09ce2237155d455aac
Masahiro Yoshimoto <yoshimoto@onsanai.com>
2026-08-02 15:10:34 +0900

    fetchData URL を GoogleDrive から Cloudflare へ変更

--- speakers3.js
+++ speakers3.js
@@ -218,7 +218,7 @@ function readData(data) {
 
 function fetchData() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://script.google.com/macros/s/AKfycbzQrE9Q5cQtcHHSb6VYg45aO95Fo6HicFklxA9-rxIqnLTG-B_zPmVvamBhyuFY0gA5/exec', true);
+    xhr.open('GET', 'https://asapoki.yoshimoto.workers.dev/', true);
     xhr.onload = function () {
         const startTime = performance.now();
         if (xhr.status === 200) {
